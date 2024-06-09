2066a7ab1ef96e6a33a4ca4bd15a5a7646460666
LeviLeach <levi.leachg@gmail.com>
2024-06-08 21:41:47 -0400

    Lots of updates

--- TypingAnimation.js
+++ TypingAnimation.js
@@ -1,17 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const textElement = document.getElementById('text');
-    const text = textElement.textContent;
-    let index = -5;
-    const speed = 125; // typing speed in milliseconds
+//    const textElement = document.getElementById('text');
+    const textElements = Array.from(document.getElementsByClassName('play-animation'));
+//    Array.from(textElements).forEach((textElement) => typeLetter(textElement));
 
-    function typeLetter() {
-        if (index < text.length) {
-            textElement.textContent = text.substring(0, index + 1);
-            index++;
-            setTimeout(typeLetter, speed);
-        }
+    for (let i = 0; i < textElements.length; i++) {
+      setTimeout(doNothing, 10000)
+      typeLetter(textElements[i])
+      setTimeout(doNothing, 5000)
     }
-
-    textElement.textContent = ''; // Clear the initial content
-    typeLetter();
 });
+
+function typeLetter(textElement) {
+    const fullText = textElement.textContent;
+    let index = -5;
+    const speed = 100;
+    textElement.textContent = '';
+    typeLetterHelper(textElement, fullText, index, speed);
+}
+
+function typeLetterHelper(textElement, fullText, index, speed) {
+    if (index < fullText.length) {
+        textElement.textContent = fullText.substring(0, index + 1);
+        index++;
+        setTimeout(typeLetterHelper, speed, textElement, fullText, index, speed);
+    }
+}
+
+function doNothing() {
+    console.log('Timeout')
+}
